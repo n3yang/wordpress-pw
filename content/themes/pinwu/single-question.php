51847c7ba753d0564fa8fd7209345e65403078b4
n3yang <n3yang@gmai.com>
2013-12-10 16:16:28 +0800

    question page is finished

--- content/themes/pinwu/single-question.php
+++ content/themes/pinwu/single-question.php
@@ -6,18 +6,66 @@
  * @subpackage Pinwu
  */
 
+$the_page_post_id = get_the_ID();
 get_header(); ?>
 
 
-<h1>question template</h1>
+	<!--
+	<div class="crumbs base-clear">
+    	<div class="crumbs-list">
+        	<span>
+            	<a href="#" class="home-icon">首页</a>
+            </span>
+        	<span>
+            	<a href="#">卧房定制家居</a>
+            </span>
+        	<span>
+            	<a href="#">新实用主义书房家具</a>
+            </span>
+        </div>
+    </div>
+	/Crumbs-->
+    
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style/products.css"/>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/products.js"></script>
+    
+	<!--ListOfProducts-->
+    <div class="main">
+    	
+        <?php echo pinwu_get_ad_banner() ?>
+                
+        <div class="products base-clear">
+        	<?php get_sidebar() ?>
+            
+<?php
+$posts = query_posts(array('post_type'=>'question', 'p'=>$the_page_post_id));
+if (have_posts()){
+	the_post();
+}
+?>
+            <div class="products-right">
+                <div class="products-list-wrap">
+                    <div class="products-list-screen qa-wrap base-clear">
+                        
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				
-				<?php the_permalink() ?>
-				<?php the_content()?>
-
-			<?php endwhile; // end of the loop. ?>
-
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+                        <div class="qa-title">
+                            <p><?php the_title() ?><span class="t-h">?</span></p>
+                        </div>
+                        <p class="time"><?php echo the_date() ?></p>
+                        <div class="qa-q">
+                            <p class="qa-q-t">咨询内容</p>
+                            <?php the_content() ?>
+                        </div>
+                        <div class="qa-a">
+                            <p class="qa-a-t">品屋回复</p>
+                            <?php the_excerpt() ?>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+    </div> 
+    <!--/ListOfProducts-->
