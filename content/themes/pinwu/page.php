bb029df91d092c7deb5a4ea623d07ad622d930de
n3yang <n3yang@gmai.com>
2014-03-15 20:15:55 +0800

    update page.php

--- content/themes/pinwu/page.php
+++ content/themes/pinwu/page.php
@@ -1,4 +1,12 @@
 <?php 
 get_header(); ?>
 
+<?php
+while ( have_posts() ) {
+	the_post();
+	get_template_part( 'content', 'page' );
+	comments_template( '', true );
+}
+?>
 
+<?php get_footer(); ?>
